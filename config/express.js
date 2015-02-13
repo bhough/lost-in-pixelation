dcb5959fec2ac30371c0fea01b0d4028b4222b3b
Brian Hough <brian.r.hough@gmail.com>
2015-02-13 15:38:58 -0800

    Add handlebars functionality, and basic template rendering for first article

--- config/express.js
+++ config/express.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var compress = require('compression');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+var exphbs  = require('express-handlebars');
 
 module.exports = function() {
 
@@ -26,6 +27,18 @@ module.exports = function() {
 
     app.use(bodyParser.json());
     app.use(methodOverride());
+
+    exhbsConfig = {
+        defaultView: './app/views/layouts/index',
+        partialsDir: './app/views/partials',
+        layoutsDir: './app/views/layouts'
+    }
+
+    app.set('views', './app/views/layouts');
+    app.engine('handlebars', exphbs(exhbsConfig));
+
+    app.set('view engine', 'handlebars');
+    require('../app/routes/index.server.routes.js')(app);
 
     app.use(express.static('./'));
 
